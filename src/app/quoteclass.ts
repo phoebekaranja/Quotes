50191851d551678efe4705a62583cbb991a04544
phoebekaranja <phoebemurish@gmail.com>
2018-09-18 00:14:52 +0300

    Add all changes

--- src/app/quoteclass.ts
+++ src/app/quoteclass.ts
@@ -1,5 +1,12 @@
 export class Quoteclass {
     constructor( public id:number, public title:string, public author:string){
-        
+         
+    }
+}
+export class Goal {
+    public showDescription:boolean;
+    constructor(public id:number, public name:string,public description:string){
+        this.showDescription=false
+
     }
 }
