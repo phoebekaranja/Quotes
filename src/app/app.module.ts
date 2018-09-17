ad54d1d3ba8ce85f41407a8b8cc048947be762d5
phoebekaranja <phoebemurish@gmail.com>
2018-09-18 00:05:41 +0300

    Add changes to files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule
